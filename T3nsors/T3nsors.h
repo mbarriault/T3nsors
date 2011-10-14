2d1b46a87700f259abf00a0cdafa603bba036c70
Michael Barriault <mikebarriault@me.com>
2011-10-14 09:41:15 -0400

    Initial Commit

---
+++ T3nsors/T3nsors.h
@@ -0,0 +1,23 @@
+/*
+ *  T3nsors.h
+ *  T3nsors
+ *
+ *  Created by Michael Barriault on 11-10-14.
+ *  Copyright (c) 2011 University of Guelph. All rights reserved.
+ *
+ */
+
+#ifndef T3nsors_
+#define T3nsors_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class T3nsors
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
