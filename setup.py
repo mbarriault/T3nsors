b2551c9f0de762aa156e4a0ef98286a32c1a2a8c
Michael Barriault <mikebarriault@me.com>
2011-10-18 22:02:20 -0400

    Adding t3py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import distutils.core as core
+
+core.setup(
+	name = 't3py',
+	description = 'T3nsors Python Additions',
+	author = 'Mike Barriault',
+	author_email = 'mikebarriault@me.com',
+	packages = ['t3py'],
+	scripts = []
+)
---
+++ t3py/__init__.py
@@ -0,0 +1,61 @@
+import h5py
+import numpy
+
+class hdf:
+	def __init__(self, file):
+		self.file = file
+		self.f = h5py.File(file, 'r')
+		self.t = sorted(self.f['data'].keys(), key=lambda x : float(x))
+		self.cnt = 0
+		F = self.f['data'][self.t[0]]
+		if type(F) is h5py._hl.dataset.Dataset:
+			self.rank = 0
+			self.shape = F.shape
+		elif type(F) is h5py._hl.group.Group:
+			self.rank = len(F.keys())
+			self.shape = F['0'].shape
+		self.x = []
+		X = self.f['coords']
+		for c in X.keys():
+			ar = numpy.arange(X[c][0], X[c][1]+X[c][2]/2, X[c][2])
+			if len(ar) < self.shape[int(c)]:
+				ar = ar[:-1]
+			self.x.append(ar)
+	def __del__(self):
+		self.f.close()
+	def __getitem__(self, index):
+		key = self.t[index]
+		F = self.f['data'][key]
+		if self.rank == 0:
+			f = numpy.array(F)
+		else:
+			f = []
+			for c in self.f['data'][key]:
+				f.append(numpy.array(F[c]))
+			f = numpy.array(f)
+		return f
+	def size(self):
+		return len(self.t)
+	def __iter__(self):
+		return self
+	def next(self):
+		try:
+			result = self[self.cnt]
+			self.cnt += 1
+			return result
+		except:
+			raise StopIteration
+	def shell(self,i,c=0):
+		if self.rank == 0:
+			f = self[i]
+		else:
+			f = self[i][c]
+		shape = f.shape[0:1]
+		s = numpy.zeros(shape)
+		for j in xrange(f.shape[1]):
+			for k in xrange(f.shape[2]):
+				s += f[:,j,k] * (self.x[0])**2 * numpy.sin(self.x[1][j])
+		s -= 0.5 * ( f[:,0,0] * (self.x[0])**2 * numpy.sin(self.x[1][0]) + f[:,-1,0] * (self.x[0])**2 * numpy.sin(self.x[1][-1]) )
+		s *= 2*math.pi * (self.x[1][-1]-self.x[1][0])/self.x[1].size
+		return s
+		
